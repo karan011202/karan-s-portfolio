533053dcc2336fbf04b28bab83a12702edb735ab
Karan Singh <karansingh06949@gmail.com>
2026-05-21 16:51:53 +0530

    Initial enterprise portfolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Projects } from './pages/projects/projects';
+import { Experience } from './pages/experience/experience';
+import { Skills } from './pages/skills/skills';
+import { Contact } from './pages/contact/contact';
+
+export const routes: Routes = [
+  { path: '', component: Home },                
+  { path: 'about', component: About },
+  { path: 'projects', component: Projects },
+  { path: 'experience', component: Experience },
+  { path: 'skills', component: Skills },
+  { path: 'contact', component: Contact },
+];
---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [CommonModule],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss',
+})
+export class Navbar {
+  isMenuOpen = false;
+
+toggleMenu() {
+  this.isMenuOpen = !this.isMenuOpen;
+}
+}
